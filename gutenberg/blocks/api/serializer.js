38fe05d53c4b9a6cc923481c77a9442e8a8c105b
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-02-25 02:54:53 +0200

    No need for absolute path

--- gutenberg/blocks/api/serializer.js
+++ gutenberg/blocks/api/serializer.js
@@ -8,7 +8,7 @@ import isEqualShallow from 'is-equal-shallow';
 /**
  * WordPress dependencies
  */
-import { Component, cloneElement, renderToString } from '../../element';
+import { Component, cloneElement, renderToString } from '@wordpress/element';
 import { hasFilter, applyFilters } from './rn/hooks';
 
 /**
